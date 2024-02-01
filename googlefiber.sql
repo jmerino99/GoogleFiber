a7f11eb89d5a32817150de3c7bd69001caf2ab9b
jmerino99 <137907629+jmerino99@users.noreply.github.com>
2024-02-01 12:16:29 -0300

    Create googlefiber.sql

---
+++ googlefiber.sql
@@ -0,0 +1,42 @@
+#union de los tres datasets
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `cursobi-410918.Fiber.market_1`
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `cursobi-410918.Fiber.market_2`
+UNION ALL
+SELECT
+  date_created,
+  contacts_n,
+  contacts_n_1,
+  contacts_n_2,
+  contacts_n_3,
+  contacts_n_4,
+  contacts_n_5,
+  contacts_n_6,
+  contacts_n_7,
+  new_type,
+  new_market
+FROM `cursobi-410918.Fiber.market_3`
